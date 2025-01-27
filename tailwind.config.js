d2e0ac4122f0e3cf367c2a45b1433bf78fcd29d6
Aashik1701 <md1704ashik@gmail.com>
2025-01-27 19:07:36 +0530

    update

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-export const content = [
-  "./index.html",
-  "./src/**/*.{js,jsx,ts,tsx}", // Adjust according to your project structure
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
