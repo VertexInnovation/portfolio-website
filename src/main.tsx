3f8d3d31e77db5be37ef8d65d703b2ceb2fe0ff4
NAbinav <abinav0506@gmail.com>
2025-02-15 16:42:41 +0530

    Authentication

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import './index.css'
-import App from './App.tsx'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import "./index.css";
+import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId="727346499975-g4j5a4h9chc0o0f48ir1fbhd6b4thu75.apps.googleusercontent.com"
+const clientId = import.meta.env.VITE_CLIENTID;
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
