b7c54e0def33e9566d0727ab7d4daf6128ce0b42
Yashvanthk05 <yashvanthk05@gmail.com>
2025-02-15 16:47:50 +0530

    Authentication

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const clientId="727346499975-g4j5a4h9chc0o0f48ir1fbhd6b4thu75.apps.googleusercontent.com"
+const clientId=import.meta.env.VITE_CLIENTID;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
